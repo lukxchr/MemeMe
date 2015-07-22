7e94c76103c2b70114649775498e280bb3878e4d
lukxchr <lukxchr@gmail.com>
2015-07-22 16:21:21 +0200

    change table cells: images are now of equal sized and aligned

--- MemeMe/MemeTableViewCell.swift
+++ MemeMe/MemeTableViewCell.swift
@@ -12,7 +12,15 @@ class MemeTableViewCell: UITableViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        //make imageViews equal-sized, image in the centre with original aspect ratio
         self.imageView!.frame = CGRectMake(0.0, 0.0, 150.0, 100.0)
+        self.imageView!.contentMode = .ScaleAspectFit
+        self.imageView!.backgroundColor = .darkGrayColor()
+        
+        //position textLabel 20p from the right edge of the imageView
+        var origin = self.textLabel!.frame.origin
+        origin.x = 170.0
+        self.textLabel!.frame.origin = origin
     }
 
 }
